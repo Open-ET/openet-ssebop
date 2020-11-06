d2fc99644f9f3e0cf99fc669edd7dc23bb1768d8
Charles Morton <charles.g.morton@gmail.com>
2020-11-06 15:32:13 -0800

    Cleanup

--- openet/ssebop/__init__.py
+++ openet/ssebop/__init__.py
@@ -2,9 +2,6 @@ from .image import Image
 from .collection import Collection
 from . import interpolate
 
-# TODO: Remove before pulling into master
-print('gridded C version')
-
 __version__ = "0.0.31"
 
 MODEL_NAME = 'SSEBOP'
